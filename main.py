f0e209fc833a56ca5050e399f44748d24e0ca76d
phanisanam <phani.sanam555@gmail.com>
2018-12-10 22:44:51 +0530

    student json file is added

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import results
 import mail
+import json
 
+def getEmail(data, hallticket):
+	for student in data:
+		if(hallticket == student['rollno']):
+			return student['email']
+def main():
 
-roll_nos={'178x1a0585':"phani.sanam555@gmail.com",'178x1a0580':"dineshyepuri05@gmail.com"}
-marks={}
- 
-for roll_no in roll_nos:
-	finalResult = results.getResults(roll_no)
-	marks[roll_no] = finalResult
+	with open('data.json')as json_file:
+	 data=json.load(json_file)
+	#roll_nos={'178x1a0585':"phani.sanam555@gmail.com",'178x1a0580':"dineshyepuri05@gmail.com"}
+	students = data['students']
+	marks={}
+	 
+	for studentIndex in students:
+		roll_no = studentIndex['rollno']
+		finalResult = results.getResults(roll_no)
+		marks[roll_no] = finalResult
 
-print(marks)
+	for roll_num in marks:
+	 print(roll_num)
+	 receiverId = getEmail(data['students'], roll_num)
+	 mail.sendGmail("phani.sanam555@gmail.com","********",receiverId,marks.get(roll_num))
 
-for roll_num in marks:
- print(roll_num)
- mail.sendGmail("phani.sanam555@gmail.com","hulknanda",roll_nos[roll_num],marks.get(roll_num))
-
+if __name__ == '__main__':
+    main()
